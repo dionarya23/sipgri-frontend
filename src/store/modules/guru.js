5e6fbd7861912744eaa681c0d7d29c4f861d33d0
BennoAlif <bennoalif41@gmail.com>
2021-04-25 22:33:50 +0700

    CRUD Guru

--- src/store/modules/guru.js
+++ src/store/modules/guru.js
@@ -4,9 +4,17 @@ const guru = {
   namespaced: true,
   state: {
     guru: [],
-    isLoading: false
+    isLoading: false,
+    alert: {
+      type: "",
+      message: ""
+    }
   },
-  mutations: {},
+  mutations: {
+    SET_GURU(state, data) {
+      state.guru = data;
+    }
+  },
   actions: {
     getAllGuru({ commit, state, rootState }) {
       state.isLoading = true;
@@ -14,19 +22,103 @@ const guru = {
         axios({
           url: "guru",
           method: "GET",
-          headers: { Authorization: rootState.token }
+          headers: { Authorization: rootState.auth.token }
         })
           .then((res) => {
-            console.log(res.data);
-            //   commit("set_users", res.data.data);
-            //   state.base_url_avatar = res.data.base_url_image;
-            //   state.isLoading = false;
+            commit("SET_GURU", res.data.data);
+            state.isLoading = false;
             resolve(res);
           })
           .catch((err) => {
             console.error(err);
-            //   state.isLoading = false;
-            //   commit("error_alert", "Failed to display users data");
+            state.isLoading = false;
+            reject(err);
+          });
+      });
+    },
+    createUser({ commit, state }, data) {
+      state.isLoading = true;
+      return new Promise((resolve, reject) => {
+        axios({ url: "guru/register", method: "POST", data })
+          .then((res) => {
+            state.alert.type = "success";
+            state.alert.message = "Berhasil menambah data guru";
+            commit("SHOW_ALERT", state.alert, { root: true });
+            state.isLoading = false;
+            resolve(res);
+          })
+          .catch((err) => {
+            console.error(err);
+            state.alert.type = "error";
+            state.alert.message = "Gagal menambah data guru";
+            commit("SHOW_ALERT", state.alert, { root: true });
+            state.isLoading = false;
+            reject(err);
+          });
+      });
+    },
+    updateUser({ commit, state }, data) {
+      state.isLoading = true;
+      return new Promise((resolve, reject) => {
+        axios({ url: `guru/${data.oldNip}`, method: "PUT", data })
+          .then((res) => {
+            state.alert.type = "success";
+            state.alert.message = "Berhasil mengubah data guru";
+            commit("SHOW_ALERT", state.alert, { root: true });
+            state.isLoading = false;
+            resolve(res);
+          })
+          .catch((err) => {
+            console.error(err);
+            state.alert.type = "error";
+            state.alert.message = "Gagal mengubah data guru";
+            commit("SHOW_ALERT", state.alert, { root: true });
+            state.isLoading = false;
+            reject(err);
+          });
+      });
+    },
+    deleteUser({ commit, state }, nip) {
+      state.isLoading = true;
+      return new Promise((resolve, reject) => {
+        axios({ url: `guru/${nip}`, method: "DELETE" })
+          .then((res) => {
+            state.isLoading = false;
+            state.alert.type = "success";
+            state.alert.message = "Berhasil menghapus data guru";
+            commit("SHOW_ALERT", state.alert, { root: true });
+            state.guru = state.guru.filter((item) => {
+              return item.nip != nip;
+            });
+            resolve(res);
+          })
+          .catch((err) => {
+            state.isLoading = false;
+            state.alert.type = "error";
+            state.alert.message = "Gagal menghapus data guru";
+            commit("SHOW_ALERT", state.alert, { root: true });
+            reject(err);
+          });
+      });
+    },
+    checkNip(_, data) {
+      return new Promise((resolve, reject) => {
+        axios({ url: "guru/check-nip", method: "POST", data })
+          .then((res) => {
+            resolve(res);
+          })
+          .catch((err) => {
+            reject(err);
+          });
+      });
+    },
+    checkEmail(_, data) {
+      return new Promise((resolve, reject) => {
+        axios({ url: "guru/check-email", method: "POST", data })
+          .then((res) => {
+            resolve(res);
+          })
+          .catch((err) => {
             reject(err);
           });
       });
